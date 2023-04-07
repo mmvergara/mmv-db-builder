cfededf0db3bcd05ef566650d08671baa227da0c
mmvergara <mark.jesusmanabat@gmail.com>
2023-04-08 06:05:16 +0800

    add Diagram Page

--- src/pages/DiagramPage.tsx
+++ src/pages/DiagramPage.tsx
@@ -1,12 +1,60 @@
 import { Box } from '@mantine/core';
 import DraggableTable from 'components/DraggableTable';
-import Xarrow, { Xwrapper } from 'react-xarrows';
+import { Xwrapper } from 'react-xarrows';
+import { Relations, TableData } from 'utilities/types/dbTypes';
+import { useState } from 'react';
+import RelationArrows from 'components/RelationArrows';
 
-export default function diagramPage() {
+export default function DiagramPage() {
+  const [tables, setTables] = useState<TableData[]>([
+    {
+      tableName: 'users',
+      columns: [
+        { columnName: 'id', columnType: 'int', columnIsKey: true },
+        { columnName: 'name', columnType: 'varchar', columnIsKey: false },
+        { columnName: 'email', columnType: 'varchar', columnIsKey: false },
+      ],
+    },
+    {
+      tableName: 'posts',
+      columns: [
+        { columnName: 'id', columnType: 'int', columnIsKey: true },
+        { columnName: 'title', columnType: 'varchar', columnIsKey: false },
+        { columnName: 'content', columnType: 'text', columnIsKey: false },
+        { columnName: 'user_id', columnType: 'int', columnIsKey: false },
+      ],
+    },
+    {
+      tableName: 'comments',
+      columns: [
+        { columnName: 'id', columnType: 'int', columnIsKey: true },
+        { columnName: 'content', columnType: 'text', columnIsKey: false },
+        { columnName: 'user_id', columnType: 'int', columnIsKey: false },
+        { columnName: 'post_id', columnType: 'int', columnIsKey: false },
+      ],
+    },
+  ]);
+  const [relations, setRelations] = useState<Relations[]>([
+    {
+      source_table: 'users',
+      source_column: 'id',
+      target_table: 'posts',
+      target_column: 'user_id',
+      relation: 'one-to-many',
+    },
+    {
+      source_table: 'users',
+      source_column: 'id',
+      target_table: 'comments',
+      target_column: 'user_id',
+      relation: 'one-to-many',
+    },
+  ]);
+  const [fontSize, setFontSize] = useState(10);
+
   return (
     <Box>
       <Box sx={{ width: '100%', backgroundColor: 'red', padding: 10 }}>
-        {' '}
         NAV BAR
       </Box>
       <Xwrapper>
@@ -14,15 +62,22 @@ export default function diagramPage() {
           sx={{
             position: 'absolute',
             overflow: 'hidden',
-            border: '2px solid green',
             height: '100%',
             width: '100%',
           }}
         >
-          <DraggableTable name="Table 1" />
-          <DraggableTable name="Table 2" />
-          <DraggableTable name="Table 4" />
-          <Xarrow path="grid" start="Table 1.yoyo" end="Table 2.yoyo" />
+          {tables.map(({ tableName, columns }) => {
+            return (
+              <DraggableTable
+                tableName={tableName}
+                columns={columns}
+                fontSize={fontSize}
+              />
+            );
+          })}
+          {relations.map((relation) => (
+            <RelationArrows relationData={relation} />
+          ))}
         </Box>
       </Xwrapper>
     </Box>
