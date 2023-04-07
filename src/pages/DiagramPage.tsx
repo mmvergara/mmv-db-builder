c24b2b31c354039377aaad555c41419aac13ec41
mmvergara <mark.jesusmanabat@gmail.com>
2023-04-08 06:28:01 +0800

    converted DBstate to useLocalStorage

--- src/pages/DiagramPage.tsx
+++ src/pages/DiagramPage.tsx
@@ -1,61 +1,35 @@
-import { Box } from '@mantine/core';
+import { Box, NumberInput } from '@mantine/core';
 import DraggableTable from 'components/DraggableTable';
 import { Xwrapper } from 'react-xarrows';
 import { Relations, TableData } from 'utilities/types/dbTypes';
-import { useState } from 'react';
 import RelationArrows from 'components/RelationArrows';
+import { initialRelations, initialTables } from 'utilities/initialData';
+import useLocalStorage from 'utilities/hooks/useLocalStorage';
 
 export default function DiagramPage() {
-  const [tables, setTables] = useState<TableData[]>([
-    {
-      tableName: 'users',
-      columns: [
-        { columnName: 'id', columnType: 'int', columnIsKey: true },
-        { columnName: 'name', columnType: 'varchar', columnIsKey: false },
-        { columnName: 'email', columnType: 'varchar', columnIsKey: false },
-      ],
-    },
-    {
-      tableName: 'posts',
-      columns: [
-        { columnName: 'id', columnType: 'int', columnIsKey: true },
-        { columnName: 'title', columnType: 'varchar', columnIsKey: false },
-        { columnName: 'content', columnType: 'text', columnIsKey: false },
-        { columnName: 'user_id', columnType: 'int', columnIsKey: false },
-      ],
-    },
-    {
-      tableName: 'comments',
-      columns: [
-        { columnName: 'id', columnType: 'int', columnIsKey: true },
-        { columnName: 'content', columnType: 'text', columnIsKey: false },
-        { columnName: 'user_id', columnType: 'int', columnIsKey: false },
-        { columnName: 'post_id', columnType: 'int', columnIsKey: false },
-      ],
-    },
-  ]);
-  const [relations, setRelations] = useState<Relations[]>([
-    {
-      source_table: 'users',
-      source_column: 'id',
-      target_table: 'posts',
-      target_column: 'user_id',
-      relation: 'one-to-many',
-    },
-    {
-      source_table: 'users',
-      source_column: 'id',
-      target_table: 'comments',
-      target_column: 'user_id',
-      relation: 'one-to-many',
-    },
-  ]);
-  const [fontSize, setFontSize] = useState(10);
+  const { s: tables, sS: setTables } = useLocalStorage<TableData[]>(
+    'tables',
+    initialTables
+  );
+  const { s: relations, sS: setRelations } = useLocalStorage<Relations[]>(
+    'relations',
+    initialRelations
+  );
+  const { s: fontSize, sS: setFontSize } = useLocalStorage<number>(
+    'fontSize',
+    10
+  );
 
   return (
     <Box>
       <Box sx={{ width: '100%', backgroundColor: 'red', padding: 10 }}>
-        NAV BAR
+        <NumberInput
+          defaultValue={fontSize}
+          onChange={(v) => setFontSize(Number(v))}
+          placeholder="Your age"
+          label="Your age"
+          withAsterisk
+        />
       </Box>
       <Xwrapper>
         <Box
