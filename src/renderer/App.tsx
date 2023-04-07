79b952cb62202afd2034bce8921be793b261bbff
mmvergara <mark.jesusmanabat@gmail.com>
2023-04-07 15:33:09 +0800

    added base

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,53 +1,13 @@
-import { MantineProvider } from '@mantine/core';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import './styles.ts';
-import 'reactflow/dist/style.css';
-import FlowPage from 'flow/ReFlowPage';
-import { Notifications } from '@mantine/notifications';
-
-function HomePage() {
-  return <FlowPage />;
-}
+import DiagramPage from 'pages/DiagramPage';
 
 export default function App() {
   return (
-    <MantineProvider
-      theme={{
-        colors: {
-          cyanGreen: [
-            '#E6F8F3',
-            '#C1EDE3',
-            '#98E1D2',
-            '#6FD4C2',
-            '#4CC7B4',
-            '#00B5A5',
-            '#009E92',
-            '#00867F',
-            '#006D6C',
-            '#004F52',
-          ],
-          'ocean-blue': [
-            '#7AD1DD',
-            '#5FCCDB',
-            '#44CADC',
-            '#2AC9DE',
-            '#1AC2D9',
-            '#11B7CD',
-            '#09ADC3',
-            '#0E99AC',
-            '#128797',
-            '#147885',
-          ],
-        },
-      }}
-    >
-      <Notifications />
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </Router>
-    </MantineProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<DiagramPage />} />
+      </Routes>
+    </Router>
   );
 }
--- src/renderer/styles.ts
+++
@@ -1,5 +0,0 @@
-import './style/flow.css';
-import './style/react-flow.scss';
-import './style/column-name.scss';
-import './style/key-icon.css';
-import './style/handle.css';
