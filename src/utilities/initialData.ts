5cb8e839e4fc0a79e047e76b7b695361f86d210a
mmvergara <mark.jesusmanabat@gmail.com>
2023-04-08 17:19:43 +0800

    changed InitialData

--- src/utilities/initialData.ts
+++ src/utilities/initialData.ts
@@ -2,46 +2,66 @@ import { Relations } from './types/dbTypes';
 
 export const initialRelations: Relations[] = [
   {
-    source_table: 'users',
-    source_column: 'id',
-    target_table: 'posts',
-    target_column: 'user_id',
-    relation: 'one-to-many',
-  },
-  {
-    source_table: 'users',
-    source_column: 'id',
-    target_table: 'comments',
-    target_column: 'user_id',
+    source_table: 'players',
+    source_column: 'player_id',
+    target_table: 'teams',
+    target_column: 'team_id',
     relation: 'one-to-many',
   },
 ];
 
-export const initialTables = [
-  {
-    tableName: 'users',
-    columns: [
-      { columnName: 'id', columnType: 'int', columnIsKey: true },
-      { columnName: 'name', columnType: 'varchar', columnIsKey: false },
-      { columnName: 'email', columnType: 'varchar', columnIsKey: false },
-    ],
-  },
-  {
-    tableName: 'posts',
-    columns: [
-      { columnName: 'id', columnType: 'int', columnIsKey: true },
-      { columnName: 'title', columnType: 'varchar', columnIsKey: false },
-      { columnName: 'content', columnType: 'text', columnIsKey: false },
-      { columnName: 'user_id', columnType: 'int', columnIsKey: false },
-    ],
-  },
-  {
-    tableName: 'comments',
-    columns: [
-      { columnName: 'id', columnType: 'int', columnIsKey: true },
-      { columnName: 'content', columnType: 'text', columnIsKey: false },
-      { columnName: 'user_id', columnType: 'int', columnIsKey: false },
-      { columnName: 'post_id', columnType: 'int', columnIsKey: false },
-    ],
-  },
-];
+export const initialTablesDBML = () => {
+  if (!window.localStorage.getItem('players-position')) {
+    window.localStorage.setItem(
+      'teams-position',
+      JSON.stringify({ x: 641, y: 114 })
+    );
+  }
+
+  if (!window.localStorage.getItem('teams-position')) {
+    window.localStorage.setItem(
+      'players-position',
+      JSON.stringify({ x: 268, y: 231 })
+    );
+  }
+  return `table players {
+    player_id int [pk]
+    name varchar
+    team_id int
+  }
+
+  table teams {
+    team_id int [pk]
+    name varchar
+  }
+`;
+};
+
+// export const initialTables = [
+//   {
+//     tableName: 'users',
+//     columns: [
+//       { columnName: 'id', columnType: 'int', columnIsKey: true },
+//       { columnName: 'name', columnType: 'varchar', columnIsKey: false },
+//       { columnName: 'email', columnType: 'varchar', columnIsKey: false },
+//     ],
+//   },
+//   {
+//     tableName: 'posts',
+//     columns: [
+//       { columnName: 'id', columnType: 'int', columnIsKey: true },
+//       { columnName: 'title', columnType: 'varchar', columnIsKey: false },
+//       { columnName: 'content', columnType: 'text', columnIsKey: false },
+//       { columnName: 'user_id', columnType: 'int', columnIsKey: false },
+//     ],
+//   },
+//   {
+//     tableName: 'comments',
+//     columns: [
+//       { columnName: 'id', columnType: 'int', columnIsKey: true },
+//       { columnName: 'content', columnType: 'text', columnIsKey: false },
+//       { columnName: 'user_id', columnType: 'int', columnIsKey: false },
+//       { columnName: 'post_id', columnType: 'int', columnIsKey: false },
+//     ],
+//   },
+// ];
